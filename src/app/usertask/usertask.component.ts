ca9e36a3d72a974856fbaf2da943b18ed0a806ee
Horia Apostol <apostol_dan1901@yahoo.com>
2024-05-31 02:11:07 +0300

    styling to shownTasks

--- src/app/usertask/usertask.component.ts
+++ src/app/usertask/usertask.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Task } from '../models/models';
+import { JwtService } from '../service/jwt.service';
 
 @Component({
   selector: 'app-usertask',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrl: './usertask.component.css'
 })
 export class UsertaskComponent {
+  @Input() task: Task;
 
+
+  isEditing: boolean = false;
+
+  constructor(private jwtService: JwtService) {}
+
+  toggleEdit(): void {
+    this.isEditing = !this.isEditing;
+  }
+
+  saveChanges(): void {
+    this.jwtService.updateTask(this.task.id, this.task).subscribe(
+      (updatedTask) => {
+        this.task = updatedTask;
+        this.isEditing = false;
+        window.alert('Task updated successfully!');
+      },
+      (error) => {
+        console.error('Error updating task', error);
+        window.alert('Failed to update the task.');
+      }
+    );
+  }
+
+  cancelEdit(): void {
+    // Optionally reset any changes here if needed
+    this.isEditing = false;
+  }
 }
